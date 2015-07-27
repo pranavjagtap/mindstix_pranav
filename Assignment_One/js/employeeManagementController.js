16561d9ae8046bb8158fa52673b3eb00145ce20f
pranavjagtap <pranav.jagtap@mindstix.com>
2015-07-27 20:18:28 +0530

    Added validations

--- Assignment_One/js/employeeManagementController.js
+++ Assignment_One/js/employeeManagementController.js
@@ -1,14 +1,34 @@
 app.controller('employeeManagementController', function($scope) {
 	$scope.empList = [];	
 	$scope.id = 1;
-	$scope.maxDate = new Date();	
+	$scope.maxDate = new Date();
+	$scope.empObj = "";	
 	$scope.saveDetails = function(empObj){
-		empObj['id'] = $scope.id;
-		if (empObj.firstName && empObj.lastName && empObj.mobileNo && empObj.gender && empObj.dob ) {
+	//	if (empObj != undefined  || empObj != "") {
+		//	$scope.output = "defined " +empObj;
+		if (empObj.firstName && empObj.lastName && empObj.mobileNo && empObj.gender && empObj.dob) {
+			empObj['id'] = $scope.id;
 			$scope.empList.push(empObj);	
 			$scope.id = $scope.id + 1;
 			$scope.clearForm();
+			$scope.isTouched(false);
+		}
+		else {
+			//$scope.output = "undefined";
+			if (!$scope.myform.firstName.$touched) {$scope.myform.firstName.$touched=true;};
+			if (!$scope.myform.lastName.$touched) {$scope.myform.lastName.$touched=true;};
+			if (!$scope.myform.mobileNo.$touched) {$scope.myform.mobileNo.$touched=true;};
+			if (!$scope.myform.gender.$touched) {$scope.myform.gender.$touched=true;};
+			if (!$scope.myform.dob.$touched) {$scope.myform.dob.$touched=true;};
 		};
+	};
+
+	$scope.isTouched = function(val){
+		$scope.myform.firstName.$touched=val;
+		$scope.myform.lastName.$touched=val;
+		$scope.myform.mobileNo.$touched=val;
+		$scope.myform.gender.$touched=val;	
+		$scope.myform.dob.$touched=val;
 	};
 
 	$scope.clearForm = function(){
